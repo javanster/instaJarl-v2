c75bde843ad567356984823607860bf5bace9042
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-06-22 22:54:15 +0200

    Update links with icons

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,68 +1,82 @@
-export const links = [
-    {
-        title: "Proton Mail",
-        link: "https://mail.proton.me/",
-        icon: "MailIcon",
-        activeDuringWorkHours: false,
-    },
-    {
-        title: "Facebook",
-        link: "https://www.facebook.com",
-        icon: "UserIcon",
-        activeDuringWorkHours: false,
-    },
-    {
-        title: "YouTube",
-        link: "https://www.youtube.com",
-        icon: "FilmIcon",
-        activeDuringWorkHours: false,
-    },
-    {
-        title: "Innsida NTNU",
-        link: "https://innsida.ntnu.no/",
-        icon: "BulletListIcon",
-        activeDuringWorkHours: true,
-    },
-    {
-        title: "Blackboard",
-        link: "https://ntnu.blackboard.com/",
-        icon: "AcademicCapIcon",
-        activeDuringWorkHours: true,
-    },
-    {
-        title: "Outlook NTNU",
-        link: "https://outlook.office.com/mail/",
-        icon: "MailIcon",
-        activeDuringWorkHours: true,
-    },
-    {
-        title: "Gitlab NTNU",
-        link: "https://gitlab.stud.idi.ntnu.no/",
-        icon: "CommandLineIcon",
-        activeDuringWorkHours: true,
-    },
-    {
-        title: "GitHub",
-        link: "https://github.com",
-        icon: "CommandLineIcon",
-        activeDuringWorkHours: true,
-    },
-    {
-        title: "ChatGPT",
-        link: "https://chat.openai.com",
-        icon: "GraphIcon",
-        activeDuringWorkHours: true,
-    },
-    {
-        title: "NRK",
-        link: "https://www.nrk.no",
-        icon: "NewspaperIcon",
-        activeDuringWorkHours: false,
-    },
-    {
-        title: "ResearchGate",
-        link: "https://www.researchgate.net",
-        icon: "BeakerIcon",
-        activeDuringWorkHours: true,
-    },
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import BallotIcon from "@mui/icons-material/Ballot";
+import BiotechIcon from "@mui/icons-material/Biotech";
+import ChatIcon from "@mui/icons-material/Chat";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import SchoolIcon from "@mui/icons-material/School";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { SvgIconTypeMap } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+
+interface Link {
+  title: string;
+  link: string;
+  icon: OverridableComponent<SvgIconTypeMap<object, "svg">>;
+  activeDuringWorkHours: boolean;
+}
+
+export const links: Link[] = [
+  {
+    title: "Proton Mail",
+    link: "https://mail.proton.me/",
+    icon: EmailIcon,
+    activeDuringWorkHours: false,
+  },
+  {
+    title: "Facebook",
+    link: "https://www.facebook.com",
+    icon: AccountCircleIcon,
+    activeDuringWorkHours: false,
+  },
+  {
+    title: "YouTube",
+    link: "https://www.youtube.com",
+    icon: YouTubeIcon,
+    activeDuringWorkHours: false,
+  },
+  {
+    title: "Innsida NTNU",
+    link: "https://innsida.ntnu.no/",
+    icon: BallotIcon,
+    activeDuringWorkHours: true,
+  },
+  {
+    title: "Blackboard",
+    link: "https://ntnu.blackboard.com/",
+    icon: SchoolIcon,
+    activeDuringWorkHours: true,
+  },
+  {
+    title: "Outlook NTNU",
+    link: "https://outlook.office.com/mail/",
+    icon: AlternateEmailIcon,
+    activeDuringWorkHours: true,
+  },
+  {
+    title: "GitHub",
+    link: "https://github.com",
+    icon: GitHubIcon,
+    activeDuringWorkHours: true,
+  },
+  {
+    title: "ChatGPT",
+    link: "https://chat.openai.com",
+    icon: ChatIcon,
+    activeDuringWorkHours: true,
+  },
+  {
+    title: "NRK",
+    link: "https://www.nrk.no",
+    icon: NewspaperIcon,
+    activeDuringWorkHours: false,
+  },
+  {
+    title: "ResearchGate",
+    link: "https://www.researchgate.net",
+    icon: BiotechIcon,
+    activeDuringWorkHours: true,
+  },
 ];
