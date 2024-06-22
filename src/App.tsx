629732c33249a33111ac7b49bcde373f2704d97a
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-06-22 23:11:33 +0200

    Add styling for backround

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { Box } from "@mui/material";
+import { colors } from "./utils/colors";
 
 function App() {
-  return <Box></Box>;
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100%",
+        backgroundColor: colors.primary,
+      }}
+    ></Box>
+  );
 }
 
 export default App;
