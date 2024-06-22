08723453f8ecaea9cfd5206035577b944a4e7f4d
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-06-22 22:42:45 +0200

    Add files for links and search urls

---
+++ src/utils/links.ts
@@ -0,0 +1,68 @@
+export const links = [
+    {
+        title: "Proton Mail",
+        link: "https://mail.proton.me/",
+        icon: "MailIcon",
+        activeDuringWorkHours: false,
+    },
+    {
+        title: "Facebook",
+        link: "https://www.facebook.com",
+        icon: "UserIcon",
+        activeDuringWorkHours: false,
+    },
+    {
+        title: "YouTube",
+        link: "https://www.youtube.com",
+        icon: "FilmIcon",
+        activeDuringWorkHours: false,
+    },
+    {
+        title: "Innsida NTNU",
+        link: "https://innsida.ntnu.no/",
+        icon: "BulletListIcon",
+        activeDuringWorkHours: true,
+    },
+    {
+        title: "Blackboard",
+        link: "https://ntnu.blackboard.com/",
+        icon: "AcademicCapIcon",
+        activeDuringWorkHours: true,
+    },
+    {
+        title: "Outlook NTNU",
+        link: "https://outlook.office.com/mail/",
+        icon: "MailIcon",
+        activeDuringWorkHours: true,
+    },
+    {
+        title: "Gitlab NTNU",
+        link: "https://gitlab.stud.idi.ntnu.no/",
+        icon: "CommandLineIcon",
+        activeDuringWorkHours: true,
+    },
+    {
+        title: "GitHub",
+        link: "https://github.com",
+        icon: "CommandLineIcon",
+        activeDuringWorkHours: true,
+    },
+    {
+        title: "ChatGPT",
+        link: "https://chat.openai.com",
+        icon: "GraphIcon",
+        activeDuringWorkHours: true,
+    },
+    {
+        title: "NRK",
+        link: "https://www.nrk.no",
+        icon: "NewspaperIcon",
+        activeDuringWorkHours: false,
+    },
+    {
+        title: "ResearchGate",
+        link: "https://www.researchgate.net",
+        icon: "BeakerIcon",
+        activeDuringWorkHours: true,
+    },
+];
---
+++ src/utils/searchUrls.ts
@@ -0,0 +1,10 @@
+export const searchBaseUrls = [
+    {
+        title: "Ecosia",
+        baseUrl: "https://www.ecosia.org/search?method=index&q=",
+    },
+    {
+        title: "Google",
+        baseUrl: "https://www.google.com/search?q=",
+    },
+];
