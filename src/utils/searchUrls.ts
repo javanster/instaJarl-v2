cc00ae7a6806b5dbdc2a1147f0568296c5637a71
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-06-27 21:56:59 +0200

    Add an additional search url

--- src/utils/searchUrls.ts
+++ src/utils/searchUrls.ts
@@ -1,10 +1,14 @@
 export const searchBaseUrls = [
-    {
-        title: "Ecosia",
-        baseUrl: "https://www.ecosia.org/search?method=index&q=",
-    },
-    {
-        title: "Google",
-        baseUrl: "https://www.google.com/search?q=",
-    },
+  {
+    title: "Ecosia",
+    baseUrl: "https://www.ecosia.org/search?method=index&q=",
+  },
+  {
+    title: "Google",
+    baseUrl: "https://www.google.com/search?q=",
+  },
+  {
+    title: "DuckDuckGo",
+    baseUrl: "https://duckduckgo.com/?t=h_&q=",
+  },
 ];
